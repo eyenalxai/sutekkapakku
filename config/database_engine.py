564ab87127df6c5c5f46a7a0227e2c9e2e33ff6a
Yest Izit <hi@dmitry.email>
2022-11-20 11:03:02 +0400

    feat: pool_pre_ping=True to fix connection was closed in the middle of operation

--- config/database_engine.py
+++ config/database_engine.py
@@ -2,4 +2,4 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 from config.database import ASYNC_DATABASE_URL
 
-async_engine: AsyncEngine = create_async_engine(url=ASYNC_DATABASE_URL, pool_size=20)
+async_engine: AsyncEngine = create_async_engine(url=ASYNC_DATABASE_URL, pool_size=20, pool_pre_ping=True)
